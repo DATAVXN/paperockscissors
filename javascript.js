044b1acd88aa4efedeed1433ced722da97e1aafc
STello <datavxn@gmail.com>
2024-04-06 13:42:20 -0400

    Add random string function

---
+++ javascript.js
@@ -0,0 +1,10 @@
+/* 
+Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
+*/
+function getComputerChoice () {
+  const items = [`Rock`, `Paper`, `Scissors`];
+  let choice = items[Math. floor(Math. random() * items.length)]
+  return choice;
+  }
+
+  console.log(getComputerChoice());
