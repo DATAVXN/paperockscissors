f2e4dc4aa0098c4da8ee478fc765c5830c7e4363
STello <datavxn@gmail.com>
2024-04-07 18:32:10 -0400

    Replaced function evaluating entries as previous one not working.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 /* 
 Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
 */
-const firstHand = prompt(`First Hand! Click ok.`);
 const items = [`rock`, `paper`, `scissors`];
-
 function getComputerChoice () {
   let computerChoice = items[Math. floor(Math. random() * items.length)].toLowerCase();
   let computerAnswer = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
@@ -26,25 +24,17 @@ function getPlayerChoice () {
 Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
 Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 */
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
 
-let playerScored = 0;
-let computerScored = 0;
-let tie = 0;
-
-function scorecard (getPlayerChoice, getComputerChoice) { 
-  if () {
-        playerScored += 1; 
-        return playerScored;
-      } else if (
-           computerScored += 1; 
-            return computerScored;
-            } else if (
-                tie += 1; 
-                return tie;
-            } 
+function compareChoices() {
+   if (playerSelection === computerSelection) {
+      console.log('Tie!')
+   } else {
+    console.log(playerSelection);
+    console.log(computerSelection);
+   }
+   
   }
 
-
-console.log(getPlayerChoice());
-console.log(getComputerChoice());
-console.log(scorecard(playerScored, computerScored));
+console.log(compareChoices());
