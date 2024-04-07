3f897c87c4e49e064ba1e1d994b9a95b3cfa0270
STello <datavxn@gmail.com>
2024-04-07 15:05:37 -0400

    Update functions output.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,14 @@
 /* 
 Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
 */
+const firstHand = prompt(`First Hand! Click ok.`);
 const items = [`rock`, `paper`, `scissors`];
 
 function getComputerChoice () {
   let computerChoice = items[Math. floor(Math. random() * items.length)].toLowerCase();
   let computerAnswer = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-  if (computerChoice === 'rock' ||
-      computerChoice === 'paper' ||
-      computerChoice === 'scissor') {
-         return (`Computer chose ${computerAnswer}!`);
-      } else {
-        return computerAnswer;
-      } 
+      return computerAnswer;
   }
-
-/*
-Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
-Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-*/
 
 function getPlayerChoice () {
   let playerChoice = prompt(`Choose Rock, Paper, or Scissor`).toLowerCase();
@@ -26,11 +16,13 @@ function getPlayerChoice () {
   if (playerChoice === 'rock' ||
       playerChoice === 'paper' ||
       playerChoice === 'scissor') {
-         return (`You chose ${playerAnswer}!`);
+         return playerAnswer;
       } else {
-         prompt(`Not a valid choice; try again!`)
+         prompt(`Not a valid choice; start over!`)
       } 
-  }
+    }
 
-console.log(getPlayerChoice ());
-console.log(getComputerChoice());
+/*
+Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
+Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+*/
